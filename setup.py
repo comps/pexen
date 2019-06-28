ff373b95875dd418dbe0a8b40913afd5c3d21b4d
Jiri Jaburek <comps@nomail.dom>
2019-06-28 18:40:40 +0200

    integrate pytest into setup.py
    Use as 'python3 setup.py test'.
    
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/comps/pexen",
     packages=["pexen"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
